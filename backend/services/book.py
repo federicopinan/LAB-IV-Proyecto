8a24c4ea2f2b32e0fdca190d6e30c8d670f77136
SaulDavid1998 <105959698+SaulDavid1998@users.noreply.github.com>
2024-06-29 19:48:53 -0300

    service folder update
    update service folder in my branch

---
+++ backend/services/book.py
@@ -0,0 +1,37 @@
+from models.book import booksmodel
+from schemas.book import booksSchema
+
+class bookService():
+    def __init__(self,db) -> None:
+        self.db=db
+
+def get_books(self):
+    result=self.dbquery(booksmodel).all()
+    return result
+
+def get_books(self,id):
+   result = self.db.query(booksmodel).filter(booksmodel.id == id).first()
+   return result 
+
+def create_book(self,book:booksSchema):
+    new_book = booksmodel(**book.dict())
+    self.db.add(new_book)
+    self.db.commit()
+    return
+
+def update_book(self, id: int, data: booksSchema):
+    book=self.db.query(booksmodel).filter(booksmodel.id==id).first()
+    book.titulo=data.titulo
+    book.autor=data.autor
+    book.isbn=data.isbn
+    book.editorial=data.editorial
+    book.categoria_id=data.categoria_id
+    self.db.commit()
+    return
+
+def delete_book(self, id: int):
+    self.db.query(booksmodel).filter(booksmodel.id == id).delete()
+    self.db.commit()
+    return
+
+
---
+++ backend/services/category.py
@@ -0,0 +1,32 @@
+from models.category import categoryModel
+from schemas.category import categorySchema
+
+class categoryService():
+    def __init__(self,db) -> None:
+        self.db=db
+
+def get_categories(self):
+    result=self.dbquery(categoryModel).all()
+    return result
+
+def get_categories(self,id):
+   result = self.db.query(categoryModel).filter(categoryModel.id == id).first()
+   return result 
+
+def create_categorie(self,category:categorySchema):
+    new_category = categoryModel(**category.dict())
+    self.db.add(new_category)
+    self.db.commit()
+    return
+
+def update_category(self, id: int, data: categorySchema):
+    categoria=self.db.query(categoryModel).filter(categoryModel.id==id).first()
+    categoria.nombre=data.nombre
+    categoria.descripcion=data.descripcion
+    self.db.commit()
+    return
+
+def delete_categorie(self, id: int):
+    self.db.query(categoryModel).filter(categoryModel.id == id).delete()
+    self.db.commit()
+    return
